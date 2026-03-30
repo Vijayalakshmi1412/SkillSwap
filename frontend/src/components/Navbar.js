2738800db776c81afd7c1a342c153f5e6538f3ea
REETHUPERIYASAMY <preethu.0306@gmail.com>
2026-03-30 16:00:59 +0530

    logo added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../assets/logo.png';
 
 const Navbar = ({ user, logout }) => {
   const navigate = useNavigate();
@@ -13,51 +14,58 @@ const Navbar = ({ user, logout }) => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          TeachEach
-        </Link>
-        <div className="navbar-tagline">From One Mind to Another</div>
-        
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link">Home</Link>
-          </li>
-          
-          {user ? (
-            <>
-              <li className="navbar-item">
-                <Link to="/dashboard" className="navbar-link">Dashboard</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/skills" className="navbar-link">Skills</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/swap-requests" className="navbar-link">Requests</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/reviews" className="navbar-link">Reviews</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/leaderboard" className="navbar-link">Leaderboard</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/profile" className="navbar-link">Profile</Link>
-              </li>
-              <li className="navbar-item">
-                <button onClick={handleLogout} className="navbar-btn">Logout</button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="navbar-item">
-                <Link to="/login" className="navbar-link">Login</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/register" className="navbar-btn">Register</Link>
-              </li>
-            </>
-          )}
-        </ul>
+        <div className="navbar-left">
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="TeachEach logo" className="navbar-logo-img" />
+            <div className="navbar-text">
+              <span className="navbar-logo">TeachEach</span>
+              <span className="navbar-tagline">From One Mind to Another</span>
+            </div>
+          </Link>
+        </div>
+
+        <div className="navbar-right">
+          <ul className="navbar-menu">
+            <li className="navbar-item">
+              <Link to="/" className="navbar-link">Home</Link>
+            </li>
+
+            {user ? (
+              <>
+                <li className="navbar-item">
+                  <Link to="/dashboard" className="navbar-link">Dashboard</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/skills" className="navbar-link">Skills</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/swap-requests" className="navbar-link">Requests</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/reviews" className="navbar-link">Reviews</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/leaderboard" className="navbar-link">Leaderboard</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/profile" className="navbar-link">Profile</Link>
+                </li>
+                <li className="navbar-item">
+                  <button onClick={handleLogout} className="navbar-btn">Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="navbar-item">
+                  <Link to="/login" className="navbar-link">Login</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/register" className="navbar-btn">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
